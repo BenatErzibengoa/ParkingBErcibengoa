d29c00537bb72ba92f0a6209f9b90ae2977005bd
Beñat <bercibengoa001@ikasle.ehu.eus>
2025-06-04 10:12:10 +0200

    Calendar actualizado y timepicker añadido

--- app/src/main/java/com/lksnext/parkingbercibengoa/view/activity/NuevaReservaActivity.java
+++ app/src/main/java/com/lksnext/parkingbercibengoa/view/activity/NuevaReservaActivity.java
@@ -3,47 +3,70 @@ package com.lksnext.parkingbercibengoa.view.activity;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
 import com.lksnext.parkingbercibengoa.databinding.ActivityNuevaReservaBinding;
 
 import java.util.Calendar;
 
 public class NuevaReservaActivity extends AppCompatActivity {
 
+    ActivityNuevaReservaBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityNuevaReservaBinding binding = ActivityNuevaReservaBinding.inflate(getLayoutInflater());
+        binding = ActivityNuevaReservaBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.fechaText.setOnClickListener(v -> {showCalendar();});
+        binding.horaComienzoText.setOnClickListener(v -> { showTimePicker(binding.horaComienzoText);});
+        binding.horaFinText.setOnClickListener(v -> { showTimePicker(binding.horaFinText);});
 
-        TextInputEditText fechaText = binding.fechaText;
 
-        fechaText.setOnClickListener(v -> {
-            final Calendar calendar = Calendar.getInstance();
-            int year = calendar.get(Calendar.YEAR);
-            int month = calendar.get(Calendar.MONTH);
-            int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-            DatePickerDialog datePickerDialog = new DatePickerDialog(
-                    NuevaReservaActivity.this,
-                    android.R.style.Theme_Holo_Light_Dialog_MinWidth, // estilo tipo popup
-                    (view, selectedYear, selectedMonth, selectedDay) -> {
-                        // Formateo dd/MM/yyyy
-                        String selectedDate = String.format("%02d/%02d/%04d", selectedDay, selectedMonth + 1, selectedYear);
-                        fechaText.setText(selectedDate);
-                    },
-                    year, month, day
-            );
 
-            // Establece fecha mínima: hoy
-            datePickerDialog.getDatePicker().setMinDate(calendar.getTimeInMillis());
+    }
 
-            // Establece fecha máximo: 7 días
-            datePickerDialog.getDatePicker().setMaxDate(calendar.getTimeInMillis() + 7 * 24 * 60 * 60 * 1000);
+    private void showCalendar(){
+        MaterialDatePicker<Long> datePicker = MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Selecciona una fecha")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .build();
 
-            // Muestra el calendario como popup centrado
-            datePickerDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-            datePickerDialog.show();
+        datePicker.addOnPositiveButtonClickListener(selection -> {
+            Calendar cal = Calendar.getInstance();
+            cal.setTimeInMillis(selection);
+            String selectedDate = String.format("%02d/%02d/%04d",
+                    cal.get(Calendar.DAY_OF_MONTH),
+                    cal.get(Calendar.MONTH) + 1,
+                    cal.get(Calendar.YEAR));
+            binding.fechaText.setText(selectedDate);
+        });
+
+        datePicker.show(getSupportFragmentManager(), "DATE_PICKER");
+    }
+
+
+    private void showTimePicker(TextInputEditText textInputEditText) {
+        MaterialTimePicker picker = new MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_12H)
+                .setHour(7)
+                .setMinute(0)
+                .setTitleText("Seleccione la hora")
+                .build();
+
+        picker.show(getSupportFragmentManager(), "time_picker");
+
+        picker.addOnPositiveButtonClickListener(v -> {
+            String amPm = picker.getHour() >= 12 ? "PM" : "AM";
+            int hour = picker.getHour() % 12;
+            if (hour == 0) hour = 12;
+
+            String formattedTime = String.format("%02d:%02d %s", hour, picker.getMinute(), amPm);
+            textInputEditText.setText(formattedTime);
         });
     }
 
