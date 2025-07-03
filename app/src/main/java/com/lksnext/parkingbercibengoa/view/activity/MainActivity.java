bd0749ba878afaad4b4ea956fb574a603b1b1150
Beñat <bercibengoa001@ikasle.ehu.eus>
2025-07-03 13:13:54 +0200

    Log out implementado

--- app/src/main/java/com/lksnext/parkingbercibengoa/view/activity/MainActivity.java
+++ app/src/main/java/com/lksnext/parkingbercibengoa/view/activity/MainActivity.java
@@ -12,6 +12,17 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.lksnext.parkingbercibengoa.R;
 import com.lksnext.parkingbercibengoa.databinding.ActivityMainBinding;
 
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.lksnext.parkingbercibengoa.configuration.SessionManager;
+import com.lksnext.parkingbercibengoa.viewmodel.ReservasViewModelFactory;
+
+
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
@@ -25,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
         //Asignamos la vista/interfaz main (layout)
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
 
         //Con el NavigationHost podremos movernos por distintas pestañas dentro de la misma pantalla
         NavHostFragment navHostFragment =
@@ -56,4 +70,37 @@ public class MainActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            //  Cerrar sesión Firebase
+            FirebaseAuth.getInstance().signOut();
+
+            //  Borrar sesión local
+            SessionManager sessionManager = new SessionManager(this);
+            sessionManager.cerrarSesion();
+
+            //Limpiar viewmodel
+            ReservasViewModelFactory.clearInstance();
+
+
+            //  Ir a LoginActivity
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 }
